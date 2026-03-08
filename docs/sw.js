713562626bf402adf4b2dfe43fe80e5f929cc7f1
이상국 <soopapa@isang-gug-ui-MacBookPro.local>
2026-03-08 21:42:40 +0900

    Add PWA support: icon, manifest, service worker

---
+++ docs/sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'rest-time-v1';
+const ASSETS = [
+  '/time/',
+  '/time/index.html',
+  '/time/manifest.json',
+  '/time/icon.svg',
+  '/time/sw.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
